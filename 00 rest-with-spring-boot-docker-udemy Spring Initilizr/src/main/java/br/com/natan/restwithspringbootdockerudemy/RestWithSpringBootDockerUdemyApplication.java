ba112e7f377b780923e0b4b35fca6733fb9403ce
Natanael <felipe.programador@gmail.com>
2022-03-21 19:59:58 -0300

    Hello SpringBoot and REST and Docker Udemy

---
+++ 00 rest-with-spring-boot-docker-udemy Spring Initilizr/src/main/java/br/com/natan/restwithspringbootdockerudemy/RestWithSpringBootDockerUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.natan.restwithspringbootdockerudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootDockerUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootDockerUdemyApplication.class, args);
+	}
+
+}
---
+++ 00 rest-with-spring-boot-docker-udemy Spring Initilizr/src/test/java/br/com/natan/restwithspringbootdockerudemy/RestWithSpringBootDockerUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.natan.restwithspringbootdockerudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootDockerUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
